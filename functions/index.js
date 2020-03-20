c095e1ef798bdd325f49b42a43a9df29f3f6cf3e
RadGade <rishiadhikari5@gmail.com>
2020-03-20 20:26:15 +1030

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+
+exports.addTeacherRole = functions.https.onCall((data, context) => {
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            teacher : true
+        })
+    }).then(() => {
+        return {
+            message : `Yayyy!! ${data.email} is a teacher`
+        }
+    }).catch((err) => {
+        return err
+    })
+})
+
+
+exports.addStudentRole = functions.https.onCall((data, context) => {
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            teacher : false
+        })
+    }).then(() => {
+        return {
+            message : `Yayyy!! ${data.email} is a student`
+        }
+    }).catch((err) => {
+        return err
+    })
+})
